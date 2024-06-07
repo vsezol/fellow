3b58e1782438c046edf55e422b95ac50f6104d74
Vsevolod Zolotov <vsezold@gmail.com>
2024-06-07 13:54:09 +0300

    fix: hide navbar in mobile conversation

--- apps/fellow/src/app/layout.tsx
+++ apps/fellow/src/app/layout.tsx
@@ -1,14 +1,22 @@
 import { Outlet } from 'react-router-dom';
+import { selectCurrentChatName } from '../entities/chat';
+import { useIsMobile } from '../shared';
+import { useAppSelector } from '../store';
 import { Navbar } from '../widgets/navbar';
 
 export default function Layout() {
+  const isMobile = useIsMobile();
+  const currentChatName = useAppSelector(selectCurrentChatName);
+
+  const isMobileConversation = isMobile && currentChatName;
+
   return (
     <div className="lg:container h-screen mx-auto md:p-4 p-0 flex md:flex-row-reverse flex-col">
       <div className="overflow-hidden flex-1 flex-grow">
         <Outlet />
       </div>
 
-      <Navbar />
+      {!isMobileConversation && <Navbar />}
     </div>
   );
 }
