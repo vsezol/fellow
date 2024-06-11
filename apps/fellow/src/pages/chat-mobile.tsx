d0ef4c02a0135dcfedcc906ce6388c82a6b23495
Vsevolod Zolotov <vsezold@gmail.com>
2024-06-12 00:27:09 +0300

    fix: build

--- apps/fellow/src/pages/chat-mobile.tsx
+++ apps/fellow/src/pages/chat-mobile.tsx
@@ -1,16 +1,15 @@
-import { selectCurrentChatName } from '../entities/chat';
-
+import { selectCurrentChatId } from '../entities/chat';
 import { AppearAnimation } from '../shared/ui/appear-animation';
 import { useAppSelector } from '../store';
 import { Conversation } from '../widgets/conversation';
 import { ConversationPreviewList } from '../widgets/conversation-preview-list';
 
 export default function ChatMobile() {
-  const currentChatName = useAppSelector(selectCurrentChatName);
+  const currentChatId = useAppSelector(selectCurrentChatId);
 
   return (
     <div className="flex flex-column gap-4 justify-center h-full overflow-hidden relative">
-      {!currentChatName && (
+      {!currentChatId && (
         <AppearAnimation>
           <div className="p-2 pb-0 h-full w-full">
             <ConversationPreviewList />
@@ -18,7 +17,7 @@ export default function ChatMobile() {
         </AppearAnimation>
       )}
 
-      {currentChatName && (
+      {currentChatId && (
         <AppearAnimation>
           <div className="bg-base-200 rounded-lg h-full w-full">
             <Conversation />
